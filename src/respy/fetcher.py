7aec22b99dae18b32d4fb30a9ca1f2aa3d3bd336
rodigu <rmoraisu@protonmail.com>
2025-07-29 15:16:45 -0300

    fetcher function refactor

--- src/respy/fetcher.py
+++ src/respy/fetcher.py
@@ -1,7 +1,7 @@
 from requests import Response, get
 
-from utils import nested_merge_dictionary
-from interfaces import (
+from .utils import nested_merge_dictionary
+from .interfaces import (
     APIEndpointConfiguration,
     EngineConfiguration,
     ResponseData,
@@ -9,19 +9,12 @@ from interfaces import (
 
 
 def fetch(
-    endpoint_configuration: APIEndpointConfiguration,
-    engine_configuration: EngineConfiguration,
+    domain: str,
+    configuration: APIEndpointConfiguration,
 ) -> ResponseData:
-    raw_api_endpoint = endpoint_configuration["api_endpoint"]
-
-    configuration = nested_merge_dictionary(
-        engine_configuration["endpoint_configuration_mapping"][raw_api_endpoint],
-        endpoint_configuration,
+    parametrized_api_endpoint = domain + configuration["api_endpoint"].format(
+        **configuration["api_endpoint_parameters"]
     )
-
-    parametrized_api_endpoint = engine_configuration["domain"] + configuration[
-        "api_endpoint"
-    ].format(**configuration["api_endpoint_parameters"])
 
     response: Response = get(
         url=parametrized_api_endpoint, **configuration["request_function_parameters"]
